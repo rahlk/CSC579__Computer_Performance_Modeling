0b129c43fde6eff2ca0e74a0ea6cfe1e34e785ec
rahlk <i.m.ralk@gmail.com>
2017-02-24 17:25:21 -0500

    Lab Machine Changes

--- simulation/sim1/Utils.py
+++ simulation/sim1/Utils.py
@@ -13,5 +13,18 @@ class Customer:
     def __init__(self):
         self.arrival_time = time()
         self.queued = False
+        self.serviced = False
         self.depart_time = self.arrival_time
         self.service_time = self.arrival_time-self.depart_time
+
+
+class Random:
+    """
+    Defines custom random number generator
+    """
+    def __init__(self): pass
+
+    def uniform(self, lo=0, hi=1):
+        """
+        Uniform distribution
+        """
