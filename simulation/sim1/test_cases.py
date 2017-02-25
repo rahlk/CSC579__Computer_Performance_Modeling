3d96b4360a15555ab5db88b89411f97d9fd2e567
rahlk <i.m.ralk@gmail.com>
2017-02-25 14:07:30 -0500

    Lab Machine Changes

--- simulation/sim1/test_cases.py
+++ simulation/sim1/test_cases.py
@@ -1,9 +1,11 @@
+from __future__ import print_function
 from Utils import Random
+
 
 
 def __test_rnd():
     rand = Random()
-    rand.rand0()
+    print(rand.rand0())
 
 if __name__ == "__main__":
     __test_rnd()
