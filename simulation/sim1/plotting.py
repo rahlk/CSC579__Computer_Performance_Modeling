1906891323123508764968278672ae06fb1b3648
rahlk <i.m.ralk@gmail.com>
2017-02-25 16:16:52 -0500

    Lab Machine Changes

--- simulation/sim1/plotting.py
+++ simulation/sim1/plotting.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plot
 
 def histogram(x, N=50, x_label="X", y_label="Y", the_title="Title"):
     # histogram of the data
-    n, bins, patches = plt.hist(x, N, normed=1, facecolor="gray", alpha=0.75)
+    n, bins, patches = plot.hist(x, N, normed=1, facecolor="gray", alpha=0.75)
     # add a 'best fit' line
     set_trace()
     z = np.polyfit(N, bins)
     p = np.poly1d(z)
-    l = plt.plot(bins, p, 'k--', linewidth=1)
+    l = plot.plot(bins, p, 'k--', linewidth=1)
 
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(the_title)
+    plot.xlabel(x_label)
+    plot.ylabel(y_label)
+    plot.title(the_title)
 
-    plt.show()
+    plot.show()
