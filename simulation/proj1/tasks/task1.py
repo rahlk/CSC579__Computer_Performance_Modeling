fa89b1d85c0947181f4479eb75e857cc6bc2fab0
rahlk <i.m.ralk@gmail.com>
2017-02-26 14:46:39 -0500

    Lab Machine Changes

--- simulation/proj1/tasks/task1.py
+++ simulation/proj1/tasks/task1.py
@@ -2,6 +2,7 @@ from __future__ import division
 from __future__ import print_function
 import os
 import sys
+import functools
 
 # Update path
 root = os.path.join(os.getcwd().split('proj1')[0], 'proj1')
@@ -29,5 +30,7 @@ def task_1_serial():
 def task_1_parallel():
     k = np.arange(0.05, 1, 0.1)
     C = (1e3, 1e5)
-    customers = simulate(l=k, )
+    pool_0 = mp.Pool
+    pool_0.map(functools.partial(l, K = 20, C=C[0], L=1), k)
+    # pool_1 = mp.Pool(functools.partial(l, K = 20, C=C[1], L=1), k)
     pass
