b32596574325be3c4b8477c293d916974762198b
rahlk <i.m.ralk@gmail.com>
2017-02-26 23:05:07 -0500

    Lab Machine Changes

--- simulation/proj1/Utils/ServerUtil.py
+++ simulation/proj1/Utils/ServerUtil.py
@@ -23,7 +23,7 @@ class Customer:
         self.service_time = self.arrival_time
 
     def get_wait_time(self):
-        self.wait_time = self.depart_time - self.arrival_time - self.service_time
+        self.wait_time = self.depart_time - self.arrival_time + self.service_time
         return self.wait_time
 
 
