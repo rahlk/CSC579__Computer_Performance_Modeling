2f721a895f97ad6aae0775780d0d457293269309
rahlk <i.m.ralk@gmail.com>
2017-02-27 14:46:40 -0500

    Lab Machine Changes

--- simulation/proj1/tasks/task2.py
+++ simulation/proj1/tasks/task2.py
@@ -16,11 +16,14 @@ from pdb import set_trace
 from Simulator import simulate
 
 rand = Random()
+# Set seed
+rand.set_seed(seed_val=12458)
 
-def customer_loss_rate(customers):
-    served = np.sum([customer.serviced for customer in customers])
-    total = len(customers)
-    return served / total
+
+def customer_loss_rate(server):
+    denied = len(server.rejected)
+    total = len(server.rejected + server.processed)
+    return denied / total
 
 
 def plot_loss_rate(x, y):
@@ -36,20 +39,25 @@ def simulate_partial_func(K, C):
     return simulate(l=0.85, server_lim=K, max_serviced=C, L=1, verbose=False)
 
 
-def task_2_serial():
-    k = np.arange(10, 101, 10)
-    C = (1e3, 1e4)
-    serviced = [map(functools.partial(
-    simulate_partial_func, C=max_serve), k) for max_serve in C]
+def task_2():
+    queue_size = np.arange(5, 101, 10)
+    C = (1e2, 1e3)
+    pool_0 = multiprocessing.Pool(processes=10)
+    CLR = []
+    for max_serve in C:
+        serviced_pool = pool_0.map(functools.partial(simulate_partial_func, C=max_serve), queue_size)
+        CLR.append([customer_loss_rate(s) for s in serviced_pool])
+
+    data = pd.DataFrame([[a,b,c] for a, b, c in zip(queue_size, CLR[0], CLR[1])], columns=["K", "CLR (C=1e3)", "CLR (C=1e5)"])
+    data.to_csv(os.path.abspath(os.path.join(root,"tasks/task2.csv")))
+
+
+def task2_plot():
+    data = pd.read_csv(os.path.abspath(os.path.join(root,"tasks/task2.csv")))
+    plot_loss_rate(data["K"], data["CLR (C=1e3)"], data["CLR (C=1e5)"], label_1="CLR (C=1e3)", label_2="CLR (C=1e5)")
     set_trace()
 
-def task_2_parallel():
-    k = np.arange(10, 101, 10)
-    C = (1e3, 1e4)
-    pool_0 = multiprocessing.Pool(processes=10)
-    serviced_pool = [pool_0.map(functools.partial(
-    simulate_partial_func, C=max_serve), k) for max_serve in C]
-    set_trace()
 
 if __name__ == "__main__":
-    task_2_parallel()
+    task_2()
+    task2_plot()
