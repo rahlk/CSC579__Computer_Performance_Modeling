f3b1d7ef11159ffa896ce8b37eb7bc519fc43e71
rahlk <i.m.ralk@gmail.com>
2017-02-26 19:04:01 -0500

    Lab Machine Changes

--- simulation/proj1/tasks/task1.py
+++ simulation/proj1/tasks/task1.py
@@ -44,7 +44,7 @@ def task_1_parallel():
     pool_0 = multiprocessing.Pool(processes=10)
     for lim in C:
         serviced_pool = pool_0.map(functools.partial(simulate, server_lim = 20, max_serviced=lim, L=1, verbose=False), rho_list)
-        CLR = [customer_loss_rate(s) for s serviced_pool]
+        CLR = [customer_loss_rate(s) for s in serviced_pool]
         plot_loss_rate(rho_list, CLR)
         # set_trace()
 
