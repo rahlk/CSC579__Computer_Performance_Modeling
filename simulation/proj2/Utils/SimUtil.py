84df3989c94431119ffe324cdb7cd0246b20b55c
rahlk <i.m.ralk@gmail.com>
2017-03-26 17:03:18 -0400

    Lab Machine Changes

--- simulation/proj2/Utils/SimUtil.py
+++ simulation/proj2/Utils/SimUtil.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from __future__ import division
 from RndUtil import Random
-
+from pdb import set_trace
 
 rand = Random()
 rand.set_seed(seed_val=1729)
@@ -23,6 +23,7 @@ class Customer:
         self.id = id
         self.arrival_time = 0
         self.queued = None
+        self.priority = None
         self.in_service = False
         self.serviced = None
         self.depart_time = self.arrival_time
@@ -46,22 +47,24 @@ class Simulation:
 
         self.rho = 1.
         self.clock = 0
-        self.lamdba = 1./3
+        self.lamdba = 0.95
         self.num_arrive = 0
         self.num_depart = 0
-        self.num_reject = len([c for c in self.queue if not c.serviced])
-        self.total_wait = sum([c.get_wait_time() for c in self.queue])
-        self.num_serviced = len([c for c in self.queue if c.serviced])
+        self.num_reject = 0
+        self.num_serviced = 0
+        self.total_wait = sum([c.get_wait_time() for c in self.customers])
 
         self.params = params if params else Params
 
-    def advance_time(self):
-        self.clock = min(self.t_arrive, self.t_depart)
 
-        if self.t_arrive <= self.t_depart:
-            self.handle_arrive_event()
-        else:
-            self.handle_depart_event()
+    @staticmethod
+    def generate_interarrival(l=1):
+        return rand.exponential(l)
+
+    @staticmethod
+    def generate_service_time(l=1):
+        return rand.exponential(l)
+
 
     def handle_arrive_event(self):
         self.num_arrive += 1  #  Increment arrival
@@ -73,18 +76,20 @@ class Simulation:
         if len(self.queue) >= self.params.K:
             c.depart_time = c.arrival_time
             self.t_depart = c.depart_time
+            self.num_reject += 1
             c.serviced = False
-            c.priority = self.params.K-len(queue)
+            c.priority = self.params.K-len(self.queue)
             self.customers.append(c)
 
         else:  #  Else add to queue
             c.serviced = True
-            self.queue.append(c)
 
+            self.queue.append(c)
 
     def handle_depart_event(self):
         self.num_depart += 1
         if len(self.queue) > 0:
+            self.num_serviced += 1
             c = self.queue.pop(0)
             c.service_time = self.generate_service_time(self.rho)
             c.depart_time = self.clock + c.service_time
@@ -93,11 +98,15 @@ class Simulation:
         else:
             self.t_depart = float("inf")
 
+    def advance_time(self):
+        self.clock = min(self.t_arrive, self.t_depart)
+        if self.t_arrive < self.t_depart:
+            self.handle_arrive_event()
+        else:
+            self.handle_depart_event()
 
-    @staticmethod
-    def generate_interarrival(l=1):
-        return rand.exponential(l)
+    def run_simulation(self, C=1e5):
+        while self.num_serviced < C:
+            self.advance_time()
 
-    @staticmethod
-    def generate_service_time(l=1):
-        return rand.exponential(l)
+        return self
--- simulation/proj2/proj2.py
+++ simulation/proj2/proj2.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
 from __future__ import division
+
+from pdb import set_trace
 from Utils.SimUtil import Simulation
+
 
 if __name__ == "__main__":
 
-    s = Simulation()
-
-    while s.num_serviced < 1000:
-        s.advance_time()
-        
+    sim = Simulation()
+    sim = sim.run_simulation(C=1e5)
+    set_trace()
